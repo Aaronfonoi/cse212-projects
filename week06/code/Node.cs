4c19585c596d58b71b1ef38addd97c20fdc74508
Aaronfonoi <aaronfonoi@gmail.com>
2026-04-10 19:56:50 -0700

    4 out 0f 5 problems passed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,8 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
@@ -34,12 +36,43 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+        return true;
+
+        if (value < Data)
+            if (Left != null)
+            return Left.Contains(value);
+
+            else
+            return false;
+        
+        if (Right != null)
+        return Right.Contains(value);
+
+        else
+
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left == null && Right == null)
+            return 1;
+
+        int leftHeight = 0;
+        if (Left != null)
+            leftHeight = Left.GetHeight();
+
+        int rightHeight = 0;
+        if (Right != null)
+            rightHeight = Right.GetHeight();
+
+        if (leftHeight > rightHeight)
+            return 1 + leftHeight;
+
+        else
+            return 1 + rightHeight;
+        //return 0; // Replace this line with the correct return statement(s)
     }
 }
